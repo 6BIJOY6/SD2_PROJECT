8695e433f05a8eae20157e278b626e9aea41bd03
hack_bi <bijoyj66@gmail.com>
2023-03-26 10:38:29 +0600

    web development project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import PageNOtFound from './pages/PageNOtFound';
+import Contact from './pages/Contact';
 
+// import Footer from './components/Layout/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  
+    <Routes>
+      <Route path="/" element ={<HomePage />}/>
+      <Route path="/about" element ={<About />}/>
+      <Route path="/contact" element ={<Contact />}/>
+      <Route path="*" element ={<PageNOtFound />}/>
+      
+    </Routes>
+
+  </>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../Style/contact.css";
+
+
+
+export default function ContactUs() {
+  return (
+    <Container className="my-3">
+      <Row ClassName="mb-5 mt-3">
+        <Col lg="8">
+          <h1 className="display-4 mb-4">Contact Me</h1>
+        </Col>
+      </Row>
+      <Row className="sec_sp">
+        <Col lg="5" className="mb-5">
+          <h3 className="color_sec py-4">Get In Touch</h3>
+          <address>
+            <strong>Email : bijoyj66@gmail.com</strong>
+            <br />
+            <br />
+            <strong>Phone : 01838489200</strong>
+          </address>
+
+          <p>
+            <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            illum dolore, neque soluta sint dolor eligendi eum doloremque!
+            Fugiat blanditiis, mollitia fuga consequatur libero veritatis
+            praesentium molestiae fugit? Magni, vero.</h4>
+          </p>
+        </Col>
+        <Col lg="7" className="d-flex align-items-center">
+          <form className="contact_form w-100">
+            <Row>
+              <Col lg="6" className="form-group">
+                <input
+                  className="form-control rounded-5"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  type="text"
+                />
+              </Col>
+
+              <Col lg="6" className="form-group">
+                <input
+                  className="form-control rounded-5"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  type="text"
+                />
+              </Col>
+            </Row>
+
+            <textarea
+              className="form-control rounded-5"
+              name="message"
+              id="message"
+              placeholder="Your Message"
+              cols="30"
+              rows="5"
+            ></textarea>
+            <br />
+            <Row>
+              <Col lg="12" className="from-group">
+              <button className="btn ac_btn" type="submit" >Drop</button>
+              </Col>
+            </Row>
+          </form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
