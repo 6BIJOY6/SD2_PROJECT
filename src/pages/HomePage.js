ff011585c5222698f75b1f096f25ca65ee69aaf6
hack_bi <bijoyj66@gmail.com>
2023-05-11 20:27:58 +0600

    logina n signin page update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { Carousel } from 'react-bootstrap'
 
 const HomePage = () => {
   return (
     <Layout>
-        <h1>
-            HomePage
-        </h1>
+        <div className="my-3">
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src="https://techbigs.com/uploads/2021/10/pc-creator-pro-pc-building-simulator-game-thumbnail.jpg"
+            alt="First slide"
+            width={"700"}
+            height="800"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src="https://www.aliteq.com/wp-content/uploads/2021/05/custom-pc-builder-nepal-Aliteq-GG-Recovered.jpg"
+            alt="2nd slide"
+            width={"700"}
+            height="800"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src="https://www.aliteq.com/wp-content/uploads/2021/02/custom-pc-build-in-nepal-aliteq-001-1.jpg"
+            alt="3rd slide"
+            width={"700"}
+            height="800"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
     </Layout>
   )
 }
--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,55 +1,21 @@
 import React from 'react'
 import Layout from './../components/Layout/Layout';
-import { NavLink } from 'react-bootstrap';
+
 
 const LogIn = () => {
   return (
     <Layout>
-    <section className="signin">
-      <div className="container mt-5">
-        <div className="signin-content">
-        <div className="signin-image">
-                <figure>
-                  <img src="https://cdn.dribbble.com/users/324533/screenshots/1419546/e-commerce.gif" alt=""></img>
-                </figure>
-                <NavLink to="/signup" className="signup-image-link">Create your Account
-                </NavLink>
-              </div>
-
-          
-          <div className="signup-form">
-            <h2 className="from-title"> Log in </h2>
-              <form className="register-form" id="register-form">
-                
-                <div className="form-group">
-                  <label htmlFor="email">
-                  <i class="zmdi zmdi-email material-icons-name"></i>
-                  </label>
-                  <input type="text" name="email" id="email" autoComplete="off" placeholder="your Email"/>
-                </div>
-
-                
-                <div className="form-group">
-                  <label htmlFor="password">
-                  <i class="zmdi zmdi-lock material-icons-name"></i>
-                  </label>
-                  <input type="password" name="password" id="password" autoComplete="off" placeholder="your password"/>
-                </div>
-
-                
-                <div className="form-group form-button">
-                  <input type="submit" name="signin" id="signin" className="form-submit" value="Log in"/>
-                </div>
-              </form>
-              </div>
-              
-            
-          
-        </div>
-
+      <div className="login-page">
+      <div className="login-container">
+        <h2 className="login-heading">Log In</h2>
+        <form className="login-form">
+          <input type="text" className="login-input" placeholder="Username" />
+          <input type="password" className="login-input" placeholder="Password" />
+          <button type="submit" className="login-button">Login</button>
+        </form>
+        <p className="signup-text">Don't have an account? <a href="/signup" className="signup-link">Sign Up</a></p>
       </div>
-
-    </section>
+    </div>
     </Layout>
   )
 }
