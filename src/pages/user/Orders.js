8b5f65c51cc9cea9f32aae560e7ed411f6569905
hack_bi <bijoyj66@gmail.com>
2023-05-21 20:16:30 +0600

    order list frontend

--- src/pages/user/Orders.js
+++ src/pages/user/Orders.js
@@ -1,22 +1,59 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
-import UserMenu from '../../components/Layout/UserMenu'
+import React from "react";
+import UserMenu from "../../components/Layout/UserMenu";
+import Layout from "./../../components/Layout/Layout";
+
+
 
 const Orders = () => {
+  
+
   return (
     <Layout>
-        <div className="container-flui p-3 m-3">
+      <div className="container-flui p-3 m-3 dashboard">
         <div className="row">
           <div className="col-md-3">
             <UserMenu />
           </div>
           <div className="col-md-9">
-            <h1>All Orders</h1>
+            <h1 className="text-center">All Orders</h1>
+            
+              
+                <div className="border shadow">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Buyer</th>
+                        <th scope="col"> date</th>
+                        <th scope="col">Payment</th>
+                        <th scope="col">Quantity</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container">
+            
+              
+                  </div>
+                </div>
+              
+            
           </div>
         </div>
       </div>
     </Layout>
-  )
-}
 
-export default Orders
+  );
+};
+
+export default Orders;
